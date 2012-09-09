41c2b31321dd373785aa7f1c9cfbd0d820249473
Masud Rahman <masud.s.rahman@gmail.com>
2012-09-09 04:07:05 -0400

    'utils' module containing a naive prime factorization algorithm

---
+++ python/utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def prime_factors(n):
+  if n < 2:
+    raise ValueError('less than 2: %d' % n)
+
+  candidate = 2
+  factors = []
+  while n != 1:
+    if n % candidate == 0:
+      n = n / candidate
+      factors.append(candidate)
+    else:
+      candidate += 1
+  return factors
+
