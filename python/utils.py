27005938082301b10ff3086819373f94258f3dab
Masud Rahman <masud.s.rahman@gmail.com>
2012-10-20 18:29:20 -0400

    Add a fibonacci generator and utility to retrieve sublists

--- python/utils.py
+++ python/utils.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import itertools
+
 def prime_factors(n):
   if n < 2:
     raise ValueError('less than 2: %d' % n)
@@ -16,3 +18,23 @@ def prime_factors(n):
 
 def triangular_number(n):
   return int((n*n + n) / 2)
+
+def fibonaccis():
+  a, b = 0, 1
+  while True:
+    yield a
+    b = a + b
+    a = b - a
+
+def fibonacci(x, y=None):
+  if x < 1:
+    raise ValueError('less than 1: %d' % x)
+
+  if y is None:
+    return list(itertools.islice(fibonaccis(), x - 1, x))[0]
+
+  if y <= x:
+    raise ValueError('not greater than %d: %d' % (x, y))
+
+  return list(itertools.islice(fibonaccis(), x - 1, y - 1))
+
